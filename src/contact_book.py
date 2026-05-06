db9300e28838ceb879bc5ab88b41007dab5a470c
Reza Omidi <m.r.omidi484@gmail.com>
2026-05-06 17:58:51 +0330

    Add files via upload

---
+++ src/contact_book.py
@@ -0,0 +1,61 @@
+class contactbook:   
+    """
+    A simple contact book application that stores contacts in memory.
+
+    Each contact has a name (key), phone number, email address, and physical address.
+    Contacts can be added, viewed, updated, and deleted.
+    """
+
+    def __init__(self):
+        self.contacts: dict = {}
+
+    def add_contact(self, name: str, phone: str, email:str, address: str):
+        if name in self.contacts:
+            print('Contact exists.')
+
+        else:
+            # self.contacts[name] = {
+            #     'phone': phone,
+            #     'email': email, 
+            #     'address': address
+            # }   
+            self.contacts[name] = {}
+            self.contacts[name]['phone'] = phone
+            self.contacts[name]['email'] = email
+            self.contacts[name]['address'] = address
+
+            print('Contact added')
+
+    def view_contacts(self):
+        for name, info in self.contacts.items():
+            print("Name:", name)
+            print("Phone:", info['phone'])
+            print("Email:", info['email'])
+            print("Address:", info['address'])
+            print('-'*50)
+
+    def update_contact(self, name:str, phone: str = None, email: str = None, address: str = None):
+        if name in self.contacts:
+            # self.contacts[name] = {
+            #     'phone': phone,
+            #     'email': email,
+            #     'address': address
+            # }
+            if phone:
+                self.contacts[name]['phone'] = phone
+            if email:    
+                self.contacts[name]['email'] = email
+            if address:
+                self.contacts[name]['address'] = address
+            print('Conract updated')    
+            return
+
+        print('Contact not found!')     
+
+    def delete_contact(self, name: str):
+        if name in self.contacts:
+            del self.contacts[name]
+            print('Contact deleted')
+            return
+        print('Contact not found')
+        
---
+++ src/main.py
@@ -0,0 +1,46 @@
+"""
+Author: Reza Omidi
+Date Created: 5/6/2025
+"""
+from contact_book import contactbook
+
+
+if __name__ == "__main__":
+    book = contactbook()
+
+    while True:
+        print("1. Add contact")
+        print("2. Edit contact")
+        print("3. View contacts")
+        print("4. Delete contact")
+        print("5. Quit")
+        user_choice = input("\nPlease choose an option: ")
+
+        if user_choice == '1':
+            name = input("\nEnter Contact name: ")
+            phone = input("Enter Contact phone: ")
+            email = input("Enter Contact email: ")
+            address = input("Enter Contact address: ")
+            book.add_contact(name, phone, email, address)
+
+        elif user_choice == '2':
+            name = input("\nEnter name of the contact to edit: ")
+            phone = input("Enter new/updated phone number or press Enter to keep unchanged: ")
+            email = input("Enter new/updated email or press Enter to keep unchanged: ")
+            address = input("Enter new/updated address or press Enter to keep unchanged: ")
+            book.update_contact(name, phone or None, email or None, address or None)
+
+        elif user_choice == '3':
+            print("\nList of Contacts:")
+            book.view_contacts()
+
+        elif user_choice == '4':
+            name = input("\nEnter name of contact to delete: ")
+            book.delete_contact(name)
+
+        elif user_choice == '5':
+            print("\nThank You for using Contact Book Application. Goodbye!")
+            break
+
+        else:
+            print("\nInvalid choice! Please try again.")
